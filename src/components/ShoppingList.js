5b0bd44636d3d733af2deefb585d8e5dc032b14c
liloulpz <lilou.lopezguilloret@gmail.com>
2022-05-19 17:42:42 +0200

    plant list add

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,26 @@
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+import { plantList } from '../datas/plantList'
 
 function ShoppingList() {
-    return (
-        <ul>
-            {plantList.map((plant, index) => (
-                <li key={`${plant}-${index}`}>{ plant }</li>
-            ))}
-        </ul>
-    )
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul>
+				{plantList.map((plant) => (
+					<li key={plant.id}>{plant.name}{plant.isBestSale && <span>🔥</span>}</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
