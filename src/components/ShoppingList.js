b5ed9d6e8dd4fd6e6ecf099249327c8d30045b04
liloulpz <lilou.lopezguilloret@gmail.com>
2022-05-21 17:28:59 +0200

    add plat item

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,6 @@
 import '../styles/ShoppingList.css'
 import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -16,12 +17,15 @@ function ShoppingList() {
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-						{plant.name}
-						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
-					</li>
-				))}
+				{plantList.map(({ id, cover, name, water, light }) => (
+						<PlantItem
+							id={id}
+							cover={cover}
+							name={name}
+							water={water}
+							light={light}
+						/>
+					))}
 			</ul>
 		</div>
 	)
